4f2082c37fb6bb776a2d5492a91b592da48a75aa
mariamjoll1 <mariahrafns@gmail.com>
2019-12-02 17:58:36 +0000

    bla

---
+++ thecontactor/src/views/ContactView/index.jsx
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import {View, Text, } from 'react-native';
+import { NavigationEvents } from 'react-navigation';
+
+class ContactView extends Component {
+    
+    render(){
+        return(
+            <View>
+                <Text>
+                    Testing2
+                </Text>
+            </View>
+        );
+    }
+}
+
+
+export default ContactView;
