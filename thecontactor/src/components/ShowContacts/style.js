cb54347c90744e528d687e3109fdda4ad6eb375f
mariamjoll1 <mariahrafns@gmail.com>
2019-12-06 21:02:51 +0000

    contact photo on front page

---
+++ thecontactor/src/components/ShowContacts/style.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+
+export default StyleSheet.create({
+    contactPhoto:{
+        width: 60,
+        height: 60,
+        borderRadius: 30,
+    },
+});
