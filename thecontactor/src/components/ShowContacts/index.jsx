62ddcca717c86d8229ab6572158e600f0b61f664
Fridrik <tryggingamidstodin@tmhf.is>
2019-12-06 15:14:17 +0000

    search2

--- thecontactor/src/components/ShowContacts/index.jsx
+++ thecontactor/src/components/ShowContacts/index.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, TouchableHighlight, ScrollView, Button } from 'react-native';
+import { View, TouchableHighlight, ScrollView } from 'react-native';
 // import GetContacts from '../GetContacts';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
@@ -13,27 +13,37 @@ import { SearchBar } from 'react-native-elements';
 
 
 class ShowContacts extends Component {
-    state = {
-        search: '',
-    };
+    constructor(props) {
+        super(props);
+        this.state = { search: '', contacts: contacts }
+    }
 
-    updateSearch = search => {
-        this.setState({ search })
-        console.log(search);
+    searchFilterFunction = ({ search }) => {
+        // Passing the inserted search text in searchBar
+        const newData = this.contacts.filter(function(item) {
+            // Applying filter for the inserted text in search bar
+            const contactData = item.name ? item.name.toUpperCase() : ''.toUpperCase();
+            const searchData = search.toUpperCase();
+            return contactData.indexOf(searchData) > -1;
+        });
+        this.setState({
+            // Setting the filtered newData to contacts
+            // After setting the data, the view should re-render
+            contacts: newData,
+            search: search
+        });
+
     }
     render(){
         const { search } = this.state;
         return(
-            <View>
-            <SearchBar
-                placeholder="Search..."
-                onChangeText={this.updateSearch}
-                value={search}
-
-            />
             <ScrollView>
+                <SearchBar
+                    placeholder="Search..."
+                    onChangeText={value => this.searchFilterFunction(value)}
+                    value={search}
+                />
                 <View style={{paddingLeft: 5, paddingRight: 5}}>
-                <Button title='CreateContact' onPress={()=> this.props.navigation.navigate('CreateContact')}></Button>
                 <Card>
                     {/* <GetContacts/> */}
                     {this.props.contacts != undefined ?
